d9c29c31c53c45bc43c2a87809e31d7b5daa288c
Andreas Janning <crusheraj@web.de>
2015-12-05 08:52:48 +0100

    Initialized the project with spring initializr

---
+++ src/main/java/de/qaware/qahappy/backend/QAhappyBackendApplication.java
@@ -0,0 +1,12 @@
+package de.qaware.qahappy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QAhappyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QAhappyBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/de/qaware/qahappy/backend/QAhappyBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package de.qaware.qahappy.backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = QAhappyBackendApplication.class)
+public class QAhappyBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
